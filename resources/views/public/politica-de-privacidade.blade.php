6d8b665ea445f5b6f4ee029fbd2f72a083b66789
Diego Del Bianco <diego@DIEGO-DEV-KALI.delbianco.emp.br>
2023-11-20 20:50:00 -0300

    autalizado politica de privacidade

--- resources/views/public/politica-de-privacidade.blade.php
+++ resources/views/public/politica-de-privacidade.blade.php
@@ -7,11 +7,53 @@
 </head>
 <body>
     <center>
-        <h1>Politica de privacidade</h1>
-        <p>A kiim é um CRM imobiliario, nos comprometemos a não repassar ou vender informações de cliente para terceiros sem a aprovação deles.</p>
-        <p>Podemos contar com Integrações de terceiros, mas sempre sob solicitação dos nossos clientes</p>
-        <p>Os dados do cliente final no CRM são de responsabilidade dos usuários que os cadastraram</p>
-        <p>Este é um produto da Agência</p>
+        <h1>Política de Privacidade do Kiim - Sistema de CRM para Imobiliárias e Corretores</h1>
+
+
+<p>Última atualização: 20/11/2023</p>
+
+<p>Bem-vindo ao Kiim, o seu parceiro em soluções de CRM para imobiliárias e corretores. Na Kiim, valorizamos a privacidade e a segurança dos dados dos nossos usuários. Esta política de privacidade detalha como coletamos, usamos, compartilhamos e protegemos suas informações pessoais ao utilizar os serviços do Kiim.</p>
+
+
+<h2>Informações Coletadas:</h2>
+
+<p>Informações de Conta: Ao se cadastrar no Kiim, coletamos informações pessoais, como seu nome, endereço de e-mail, número de telefone e outras informações necessárias para criar e gerenciar sua conta.</p>
+
+<p>Informações do Imóvel: Para fornecer um serviço eficaz, podemos coletar informações relacionadas a propriedades, transações imobiliárias e preferências do usuário.</p>
+
+<p>Informações de Uso: Coletamos dados sobre como você interage com o Kiim, incluindo páginas acessadas, recursos utilizados e informações sobre o dispositivo utilizado.</p>
+
+<h2>Uso das Informações:</h2>
+
+<p>Melhoria do Serviço: Utilizamos as informações coletadas para aprimorar continuamente nossos serviços, adaptando-os às suas necessidades e oferecendo uma experiência personalizada.</p>
+
+<p>Comunicação: Podemos enviar comunicações relacionadas ao serviço, como atualizações, notificações e informações importantes sobre sua conta.</p>
+
+<p>Compartilhamento de Informações: Não compartilhamos suas informações pessoais com terceiros, exceto quando necessário para fornecer os serviços contratados por você ou quando exigido por lei.</p>
+
+<h2>Segurança:</h2>
+
+<p>Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado, alteração, divulgação ou destruição não autorizada.</p>
+
+<h2>Cookies e Tecnologias Semelhantes:</h2>
+
+<p>Utilizamos cookies e tecnologias semelhantes para melhorar a experiência do usuário, analisar o tráfego e personalizar conteúdo. Ao utilizar o Kiim, você concorda com o uso dessas tecnologias.</p>
+
+<h2>Seus Direitos:</h2>
+
+<p>Você tem o direito de acessar, corrigir, atualizar ou excluir suas informações pessoais. Para exercer esses direitos ou para quaisquer dúvidas sobre nossa política de privacidade, entre em contato conosco sac@kiim.com.br</p>
+
+<h2>Alterações nesta Política:</h2>
+
+<p>Reservamo-nos o direito de modificar esta política de privacidade a qualquer momento. Recomendamos que você revise regularmente para estar ciente de quaisquer alterações.</p>
+
+<p>Ao usar o Kiim, você concorda com os termos desta política de privacidade.</p>
+
+<h2>Obrigado por escolher o Kiim!</h2>
+
+
+
+
     </center>
 </body>
 </html>
